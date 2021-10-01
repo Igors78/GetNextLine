4c496e4cea9faee7e67812e28d934fdce50e42ff
Igors78 <oleinikovsigors@gmail.com>
2021-10-01 21:56:15 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Get Next Line
+
+![image](page1_1.jpg)
+![image](page2_1.jpg)
+![image](page3_1.jpg)
+![image](page4_1.jpg)
+![image](page5_1.jpg)
+![image](page6_1.jpg)
+![image](page7_1.jpg)
---
+++ get_next_line_bonus.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ioleinik <ioleinik@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/06 20:10:03 by ioleinik          #+#    #+#             */
+/*   Updated: 2021/06/10 17:20:20 by ioleinik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int		get_next_line(int fd, char **line);
+char	*ft_strchr(const char *str, int ch);
+size_t	ft_strlen(char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *src);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
